462ab9313511759f852ae5ce6bae637bcf86b712
Subhadhriti Maikap <subhadhriti.maikap@tatadigital.com>
2022-04-27 12:11:37 +0530

    firt commit

---
+++ getOffers/src/main/java/com/springmongowrapper/getOffers/GetOffersApplication.java
@@ -0,0 +1,13 @@
+package com.springmongowrapper.getOffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetOffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetOffersApplication.class, args);
+	}
+
+}
---
+++ getOffers/src/test/java/com/springmongowrapper/getOffers/GetOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmongowrapper.getOffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetOffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
